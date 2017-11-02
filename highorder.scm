305bd4774446fb1dc4d9b608c97d393580a79ea6
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-11-02 09:47:42 +0200

    Още примери за приложения на accumulate

--- highorder.scm
+++ highorder.scm
@@ -31,21 +31,30 @@
   (accumulate-i op 0 1 (1+ n) id 1+))
 
 (define (fact n)
-  (accumulate * 1 1 n (lambda (i) i) (lambda (i) (+ i 1))))
+  (accumulate * 1 1 n id 1+))
 
 (define (pow x n)
-  (accumulate * 1 1 n (lambda (i) x) (lambda (i) (+ i 1))))
+  (accumulate * 1 1 n (lambda (i) x) 1+))
 
 (define (my-exp x n)
-  (accumulate + 0 0 n (lambda (i) (/ (pow x i) (fact i))) (lambda (i) (+ i 1))))
+  (accumulate + 0 0 n (lambda (i) (/ (pow x i) (fact i))) 1+))
 
 ;(define (my-exp x n)
 ;   (accumulate (lambda (a b) (+ a (/ (* b x) i))) 0 1 n (lambda (i) (/ 1 i))
 ;              (lambda (i) (+ i 1))))
 
-(define (my-exp a b x)
-  (if (> a b) 0
-      (+ 1 (/ (* (my-exp (+ a 1) b x) x) a))))
 
-(define (my-exp n x)
-  (accumulate (lambda (a b) (+ 1 (* a b))) 0 1 n (lambda (i) (/ x i) (lambda (i) (+ i 1))))
+
+
+(define (my-exp x n)
+  ;(accumulate (lambda (u v) (+ 1 (* x u v))) 0 1 n (lambda (i) (/ 1 i)) 1+))
+  (accumulate (lambda (u v) (+ 1 (* u v))) 0 1 n (lambda (i) (/ x i)) 1+))
+
+(define (exists a b p?)
+  (accumulate (lambda (u v) (or u v)) #f a b p? 1+))
+
+(define (prime? n)
+  (define (divisor? k) (= (remainder n k) 0))
+  (and (> n 1)
+       (not (exists 2 (sqrt n) divisor?))))
+
